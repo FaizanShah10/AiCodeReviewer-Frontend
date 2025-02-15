b4d1f72fc000401735b3a32fe57e730ac0aaca1b
FaizanShah10 <sfn.404@gmail.com>
2025-02-15 14:11:30 +0500

    Added Logo in navbar and tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { RiRobot3Fill } from "react-icons/ri";
 const Navbar = () => {
   return (
     <div>
         <nav className="w-full bg-transparent pt-4 px-6 flex justify-between items-center">
-            <h4 className="text-white fw-bold">CodeGenie</h4>
+            <div>
+              <h4 className="text-white fw-bold flex items-center gap-1"> <span><RiRobot3Fill /></span> CodeGenie</h4>
+            </div>
             <p className="text-white cursor-pointer hover:underline">Help</p>
       </nav>
     </div>
